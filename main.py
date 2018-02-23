f0672352497bb3a456e315e201413aafc94b4d95
marc wang <marcwang17@hotmail.com>
2018-02-22 22:13:31 -0500

    added mouse Highlight

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from PyQt5 import QtWidgets
 import vtk
 from uiPythonFile.ui_Versa3dMainWindow import Ui_Versa3dMainWindow
+from GUI.MouseInteractorHighLightActor import MouseInteractorHighLightActor
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -12,13 +13,24 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_Versa3dMainWindow()
         self.ui.setupUi(self)
         
-        self.ren = vtk.vtkRenderer()
-        self.ui.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
-        self.Iren = self.ui.vtkWidget.GetRenderWindow().GetInteractor()
+        self.StlRenderer = vtk.vtkRenderer()
+        self.ui.vtkWidget.GetRenderWindow().AddRenderer(self.StlRenderer)
+        self.StlInteractor = self.ui.vtkWidget.GetRenderWindow().GetInteractor()
+
+        style = MouseInteractorHighLightActor()
+        style.SetDefaultRenderer(self.StlRenderer)
+
+        self.StlInteractor.SetInteractorStyle(style)
         
+        self.ImageRenderer = vtk.vtkRenderer()
+        self.ui.slice_viewer.GetRenderWindow().AddRenderer(self.StlRenderer)
+        self.ui.ImageInteractor = self.ui.slice_viewer.GetRenderWindow().GetInteractor()
+
+        self.importedObject = []
+                
         self.ui.actionImport_STL.triggered.connect(self.import_stl)
         self.ui.SliceButton.clicked.connect(self.slice_stl)
-        self.Iren.Initialize()
+        self.StlInteractor.Initialize()
     
     def import_stl(self):
         
@@ -28,6 +40,8 @@ class MainWindow(QtWidgets.QMainWindow):
         
         reader.SetFileName(fileName[0])
         reader.Update()
+
+        self.importedObject.append(reader.GetOutput())
         
         mapper = vtk.vtkPolyDataMapper()
         mapper.SetInputConnection(reader.GetOutputPort())
@@ -35,17 +49,18 @@ class MainWindow(QtWidgets.QMainWindow):
         actor = vtk.vtkActor()
         actor.SetMapper(mapper)
         
-        self.ren.AddActor(actor)
+        self.StlRenderer.AddActor(actor)
         
-        self.ren.ResetCamera()
+        self.StlRenderer.ResetCamera()
     
     def slice_stl(self):
 
         print("start \n")
 
-        
 
-        
+
+
+
         
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
