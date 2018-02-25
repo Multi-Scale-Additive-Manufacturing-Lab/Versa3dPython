008c1a73d77465805a52c0a699a716b31702c601
marc wang <marcwang17@hotmail.com>
2018-02-25 12:31:01 -0500

    added test but import doesn't work

---
+++ test/t01_config.py
@@ -0,0 +1,50 @@
+import unittest
+import lib
+from lib.versa3dConfig import config
+from pathlib import Path
+import os
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        self.testFileName = 'test.ini'
+
+    def test_ConfigCreation(self):
+        configObject = config(self.testFileName)
+
+        IniFile = Path(self.testFileName)
+        #check if file created
+        self.assertEqual(True, IniFile.is_file())
+    
+    def test_ConfigGetValue(self):
+        configObject = config(self.testFileName)
+        value = configObject.getValue('Unit')
+
+        self.assertEqual('mm',value)
+    
+    def test_ModifyConfig(self):
+        configObject = config(self.testFileName)
+        configObject.setValue('Unit', 'inch')
+
+        value = configObject.getValue('Unit')
+
+        self.assertEqual('inch', value)
+
+    def test_SaveConfig(self):
+        configObject = config(self.testFileName)
+        configObject.setValue('Unit', 'inch')
+
+        configObject.saveConfig()
+
+        configObject2 = config(self.testFileName)
+        value = configObject.getValue('Unit')
+        self.assertEqual('inch', value)
+
+    def tearDown(self):
+        os.remove(self.testFileName)
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+
