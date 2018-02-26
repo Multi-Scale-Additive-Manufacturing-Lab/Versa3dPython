79104b1123c4fb51aac13e02cfbeb8e887ef6b60
marc wang <marcwang17@hotmail.com>
2018-02-25 23:31:57 -0500

    added scene but plane hides grid

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from lib.command import stlImportCommand
 from lib.versa3dConfig import config , FillEnum
 
 from collections import deque
+import numpy as np
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -41,9 +42,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.actionUndo.triggered.connect(self.undo)
         self.ui.actionRedo.triggered.connect(self.redo)
 
+        self.setUpScene()
+
         self.StlInteractor.Initialize()
 
         self.populateComboBox(FillEnum, self.ui.inFillComboBox)
+
     
     def import_stl(self):
         importer = stlImportCommand(self.StlRenderer,self)
@@ -66,6 +70,74 @@ class MainWindow(QtWidgets.QMainWindow):
     def populateComboBox(self,list,combobox):
         combobox.addItems(list)
     
+    def setUpScene(self):
+
+        #add coordinate axis
+        axesActor = vtk.vtkAxesActor()
+        axesActor.SetShaftTypeToLine()
+        axesActor.SetTipTypeToCone()
+        printBedSize = self._config.getValue('printbedsize')
+        buildBedHeight = self._config.getValue('buildheight')
+        axesActor.SetTotalLength(printBedSize[0],printBedSize[1],buildBedHeight)
+
+        if(printBedSize[0] <50 or  printBedSize[1] <50):
+            Increment = 1.0
+        else:
+            Increment = 10.0
+        
+        #create xy plane
+        xyPlane = vtk.vtkPlaneSource()
+        xyPlane.SetOrigin(0.0,0.0,0.0)
+        xyPlane.SetPoint1(printBedSize[0],0,0)
+        xyPlane.SetPoint2(0,printBedSize[1],0)
+        
+        xyPlaneMapper = vtk.vtkPolyDataMapper()
+        xyPlaneMapper.SetInputConnection(xyPlane.GetOutputPort())
+
+        xyPlaneActor = vtk.vtkActor()
+        xyPlaneActor.SetMapper(xyPlaneMapper)
+        xyPlaneActor.SetPickable(False)
+        xyPlaneActor.SetDragable(False)
+        xyPlaneActor.GetProperty().SetColor(0.2,0.29,0.7)
+        
+        #create grid
+        for i in np.arange(0, printBedSize[0], Increment):
+            line = vtk.vtkLineSource()
+            line.SetPoint1(i,0,0)
+            line.SetPoint2(i,printBedSize[1],0)
+            line.Update()
+
+            lineMapper = vtk.vtkPolyDataMapper()
+            lineMapper.SetInputConnection(line.GetOutputPort())
+
+            lineActor = vtk.vtkActor()
+            lineActor.SetMapper(lineMapper)
+            lineActor.SetPickable(False)
+            lineActor.SetDragable(False)
+
+            self.StlRenderer.AddActor(lineActor)
+        
+        for j in np.arange(0, printBedSize[0], Increment):
+            line = vtk.vtkLineSource()
+            line.SetPoint1(0,j,0)
+            line.SetPoint2(printBedSize[0],j,0)
+            line.Update()
+
+            lineMapper = vtk.vtkPolyDataMapper()
+            lineMapper.SetInputConnection(line.GetOutputPort())
+
+            lineActor = vtk.vtkActor()
+            lineActor.SetMapper(lineMapper)
+            lineActor.SetPickable(False)
+            lineActor.SetDragable(False)
+
+            self.StlRenderer.AddActor(lineActor)
+
+        self.StlRenderer.AddActor(xyPlaneActor)
+        self.StlRenderer.AddActor(axesActor)
+
+        self.StlRenderer.ResetCamera()
+            
     def slice_stl(self):
 
         print("start \n")
