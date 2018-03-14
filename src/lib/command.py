6c51e733e1a18f5ff10687274b1dbc0b3e68fea4
marc wang <marcwang17@hotmail.com>
2018-03-13 23:20:38 -0400

    when add stl, the shape is positioned in the middle and bottom touches the xy plane

--- src/lib/command.py
+++ src/lib/command.py
@@ -14,26 +14,44 @@ class stlImportCommand():
 
     def execute(self):
         fileName = QtWidgets.QFileDialog.getOpenFileName(self._parentWidget, 'Open stl' ,"", "stl (*.stl)")
+        if(fileName[0] != ''):
+            reader = vtk.vtkSTLReader()
+            reader.SetFileName(fileName[0])
+            reader.Update()
 
-        reader = vtk.vtkSTLReader()
-        reader.SetFileName(fileName[0])
-        reader.Update()
+            mapper = vtk.vtkPolyDataMapper()
+            mapper.SetInputConnection(reader.GetOutputPort())
 
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
+            self._actor = vtk.vtkActor()
+            self._actor.SetMapper(mapper)
 
-        self._actor = vtk.vtkActor()
-        self._actor.SetMapper(mapper)
+            printBedSize = self._config.getValue("printbedsize")
+            zRange = self._actor.GetZRange()
 
-        key = self._config.getKey("Type","Actor")
-        info = vtk.vtkInformation()
-        info.Set(key,"PolyData")
-        self._actor.GetProperty().SetInformation(info)
+            newPosition = [0]*3
 
-        self._renderer.AddActor(self._actor)
-        self._renderer.ResetCamera()
+            oldPosition = self._actor.GetPosition()
+            newPosition[0] = printBedSize[0]/2
+            newPosition[1] = printBedSize[1]/2
 
-        return self._actor
+            if(zRange[0]<0):
+                newPosition[2] = oldPosition[2]-zRange[0]
+            else:
+                newPosition[2] = oldPosition[2]
+            
+            self._actor.SetPosition(newPosition)
+
+            key = self._config.getKey("Type","Actor")
+            info = vtk.vtkInformation()
+            info.Set(key,"PolyData")
+            self._actor.GetProperty().SetInformation(info)
+
+            self._renderer.AddActor(self._actor)
+            self._renderer.ResetCamera()
+
+            return self._actor
+        else:
+            return None
         
     def undo(self):
         self._renderer.RemoveActor(self._actor)
