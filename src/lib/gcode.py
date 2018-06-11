e892835d4ec4a00e379589c3456d80460f70e92a
marc wang <marcwang17@hotmail.com>
2018-06-11 14:33:41 -0400

    fixed gcode syntax error

--- src/lib/gcode.py
+++ src/lib/gcode.py
@@ -128,7 +128,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
             self.generateGCodeLayer(count,listOfImg,imageFolder)
 
-            xmlFileName = "layer_%d.xml"%(count)
+            xmlFileName = "layer.%d.xml"%(count)
             count = count + 1
             xmlFullPath = os.path.join(self._Folderpath,xmlFileName)
 
@@ -190,84 +190,82 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
                 #step 0 - turn ON printhead and get ready to print buffer 0
                 textStr = "\"%T{}{}\"".format(str(fontNumber).zfill(2),listOfAlphabet[fontNumber-1])
-                step0 = self.ImtechPrintHead(1,8,1,0,0,BNumber,0,textStr,self.DefaultPrintHeadAddr,imgPath)
+                step0 = self.ImtechPrintHead(True,8,1,0,0,BNumber,0,textStr,self.DefaultPrintHeadAddr,imgPath)
                 self.makeStep(defaultStep,step0)
                 BNumber = BNumber + 1
                 fontNumber = fontNumber + 1
                 listTxtToPrint.append(textStr)
 
         #step 1 - move gantry to X1 = 0 
-        step1 = self.Gantry(1,0,3,[0,0],0,self.gantryXYVelocity[0],"")
+        step1 = self.Gantry(True,0,3,[0,0],0,self.gantryXYVelocity[0],"")
         self.makeStep(defaultStep,step1)
 
         #step 2 - move to Y = 0
-        step2 = self.Gantry(1,0,3,[0,0],2,self.gantryXYVelocity[1],"")
+        step2 = self.Gantry(True,0,3,[0,0],2,self.gantryXYVelocity[1],"")
         self.makeStep(defaultStep,step2)
 
         #step 3 - turn on roller
-        step3 = self.Roller(1,6,1,self.rollerRotVel)
+        step3 = self.Roller(True,6,1,self.rollerRotVel)
         self.makeStep(defaultStep,step3)
         
         #step 4 - material handling raise feed bed by = H+T+S
-        step4 = self.MaterialHandling(1,2,2,self.H+self.Thickness+self.S,self.FeedBedSel,self.feedBedVelocity,0,0)
+        step4 = self.MaterialHandling(True,2,2,self.H+self.Thickness+self.S,self.FeedBedSel,self.feedBedVelocity,0,0)
         self.makeStep(defaultStep,step4)
-
+        
         #step 5 - material handling raise build bed by = H-T
-        step5 = self.MaterialHandling(1,2,2,self.H-self.Thickness,3,self.buildBedVelocity,0,0)
+        step5 = self.MaterialHandling(True,2,2,self.H-self.Thickness,3,self.buildBedVelocity,0,0)
         self.makeStep(defaultStep,step5)
-
+        
         #step 6 - spread the powder by moving in X-coordinate 300
-        step6 = self.Gantry(1,0,3,[300,0],0,self.rollerLinVel,"")
+        step6 = self.Gantry(True,0,3,[300,0],0,self.rollerLinVel,"")
         self.makeStep(defaultStep,step6)
-
+        
         #step 7 - turn off roller
-        step7 = self.Roller(1,6,1,0)
+        step7 = self.Roller(True,6,1,0)
         self.makeStep(defaultStep,step7)
-
+        
         #step 8 - lower build bed by -(H+W)
-        step8 = self.MaterialHandling(1,2,2,(-1)*(self.H+self.W),3, self.buildBedVelocity,0,0)
+        step8 = self.MaterialHandling(True,2,2,(-1)*(self.H+self.W),3, self.buildBedVelocity,0,0)
         self.makeStep(defaultStep,step8)
-
+        
         #step 9 - lower feed bed by -(H+W)
-        step9 = self.MaterialHandling(1,2,2,(-1)*(self.H+self.W),self.FeedBedSel,self.feedBedVelocity,0,0)
-        self.makeStep(defaultStep,step4)
-
+        step9 = self.MaterialHandling(True,2,2,(-1)*(self.H+self.W),self.FeedBedSel,self.feedBedVelocity,0,0)
+        self.makeStep(defaultStep,step9)
+        
         for i in range(0,BNumber):
 
             #step 10 allign printhead with the printing area - move to lower left corner of image
-            step10 = self.Gantry(1,0,3,[0,58-origin[0]],2,self.gantryXYVelocity[1],"")
+            step10 = self.Gantry(True,0,3,[0,58-origin[0]],2,self.gantryXYVelocity[1],"")
             self.makeStep(defaultStep,step10)
 
             #step 11 allign printhead with the printing area - move to X=10
-            step11 = self.Gantry(1,0,3,[20-origin[1],0],0,self.gantryXYVelocity[0],"")
+            step11 = self.Gantry(True,0,3,[20-origin[1],0],0,self.gantryXYVelocity[0],"")
             self.makeStep(defaultStep,step11)
 
             #step 12 turn ON printhead and get ready to print buffer 0
-            step12 = self.ImtechPrintHead(1,8,5,0,0,i,0,listTxtToPrint[i],self.DefaultPrintHeadAddr)
+            step12 = self.ImtechPrintHead(True,8,5,0,0,i,0,0,0,1)
             self.makeStep(defaultStep,step12)
 
             #step 13 execute printing motion in Y direction - move to right
-            step13 = self.Gantry(1,0,3,[0,38],2,self.DefaultPrintVelocity,"")
+            step13 = self.Gantry(True,0,3,[0,38],2,self.DefaultPrintVelocity,"")
             self.makeStep(defaultStep,step13)
         
         #step 14 move back to origin in Y -direction Y=0(former step 16)
-        step14 = self.Gantry(1,0,3,[0,0],2,self.gantryXYVelocity[1],"")
+        step14 = self.Gantry(True,0,3,[0,0],2,self.gantryXYVelocity[1],"")
         self.makeStep(defaultStep,step14)
 
-        #step 15 move back to origin in X-direction X=0 (former stp 18)
-        step15 = self.Gantry(1,0,3,[0,0],0,self.gantryXYVelocity[0],"")
+        #step 15 move back to origin in X-direction X=0 
+        step15 = self.Gantry(True,0,3,[0,0],0,self.gantryXYVelocity[0],"")
         self.makeStep(defaultStep,step15)
         
         #step 16 raise build bed
-        step16 = self.MaterialHandling(1,2,2,2*self.W, 3,self.buildBedVelocity, 0,0)
+        step16 = self.MaterialHandling(True,2,2,self.W, 3,self.buildBedVelocity, 0,0)
         self.makeStep(defaultStep,step16)
 
         #step 17 raise feed bed
-        step17 = self.MaterialHandling(1,2,2,2*self.W, self.FeedBedSel,self.feedBedVelocity, 0,0)
+        step17 = self.MaterialHandling(True,2,2,self.W, self.FeedBedSel,self.feedBedVelocity, 0,0)
         self.makeStep(defaultStep,step17)
 
-
-    
     def makeStep(self, default_step, Command):
         NameNode = Command.find("Name")
         Name = NameNode.text
@@ -333,7 +331,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
         for ChoiceTxt in listOfChoice:
             root.append(E.Choice(ChoiceTxt))
         
-        root.append(E.Val(str(Val)))
+        root.append(E.Val(str(int(Val))))
 
         return root
 
@@ -408,7 +406,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
         root = (E.Cluster(
                     E.Name(Name),
-                    E.NumElts(str(NumElts))
+                    E.NumElts(str(int(NumElts)))
                 )
             )
             
@@ -423,13 +421,13 @@ class gcodeWriterVlaseaBM(gcodeWriter):
         return root
 
     def Boolean(self,Name,Val):
-        return self._TypeNameVal("Boolean",Name,Val)
+        return self._TypeNameVal("Boolean",Name,int(Val))
     
     def DBL(self,Name,Val):
-        return self._TypeNameVal("DBL",Name,Val)
+        return self._TypeNameVal("DBL",Name,float(Val))
     
     def I16(self,Name,Val):
-        return self._TypeNameVal("I16",Name,Val)
+        return self._TypeNameVal("I16",Name,int(Val))
     
     def Path(self,Name,Val):
         return self._TypeNameVal("Path", Name,Val)
@@ -437,7 +435,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
     def String(self,Name,Val):
         return self._TypeNameVal("String", Name,Val)
 
-    def XaarPrinthead(self,Bool_Xaar=0,Module=0,Function=0,Img_Path=0,Start_Stop_Print=0):
+    def XaarPrinthead(self,Bool_Xaar=False,Module=0,Function=0,Img_Path=0,Start_Stop_Print=0):
         root = self.Cluster("Printhead",4)
         root.append(self.Boolean("Printhead",Bool_Xaar))
 
@@ -452,7 +450,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
         return root
         
-    def ImtechPrintHead(self,Bool_Imtech=0,Module=0,Function=0,Voltage=0,pulse_width=0,Buffer_Number=0, VPP_On_Off=0,Imtech_txtStr=0,PrintHeadAddr=0, img_path=""):
+    def ImtechPrintHead(self,Bool_Imtech=False,Module=0,Function=0,Voltage=0,pulse_width=0,Buffer_Number=0, VPP_On_Off=0,Imtech_txtStr=0,PrintHeadAddr=0, img_path=""):
         root = self.Cluster("Printhead 2",4)
         
         root.append(self.Boolean("Imtech Printer",Bool_Imtech))
@@ -472,7 +470,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
         root.append(ImtechConfig)
         return root
     
-    def Syringe(self,Bool_Syringe=0,Module=0,Function=0,Bool_Extrusion=0,Syringe_Velocity=0):
+    def Syringe(self,Bool_Syringe=False,Module=0,Function=0,Bool_Extrusion=0,Syringe_Velocity=0):
         root = self.Cluster("Syringe",4)
         root.append(self.Boolean("Syringe",Bool_Syringe))
 
@@ -487,7 +485,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
         return root
     
-    def Syringe2(self,Bool_Syringe=0, Module=0,Function=0,Pressure_Units=0,Pressure=0,Vacuum_Units=0,Vacuum=0,Start_Stop=0):
+    def Syringe2(self,Bool_Syringe=False, Module=0,Function=0,Pressure_Units=0,Pressure=0,Vacuum_Units=0,Vacuum=0,Start_Stop=0):
         root = self.Cluster("Syringe 2",4)
         root.append(self.Boolean("Syringe 2",Bool_Syringe))
 
@@ -505,7 +503,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
         
         return root
 
-    def Gantry(self, Bool_Gantry=0, Module=0,Function=0,XYCoord=[0,0],Motor=0,MotorVelocity=0,file_path="FilePath"):
+    def Gantry(self, Bool_Gantry=False, Module=0,Function=0,XYCoord=[0,0],Motor=0,MotorVelocity=0,file_path="FilePath"):
 
         root = self.Cluster("Gantry",4)
 
@@ -525,7 +523,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
         return root
     
-    def Roller(self,Bool_Roller=0,Module=0,Function=0,Roller_Velocity=0):
+    def Roller(self,Bool_Roller=False,Module=0,Function=0,Roller_Velocity=0):
         root = self.Cluster("Roller",4)
         root.append(self.Boolean("Roller",Bool_Roller))
 
@@ -539,7 +537,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
         return root
 
-    def MaterialHandling(self,Bool_Mat=0, Module=0,Function=0,Height=0,Bed_Selection=0,Motor_Velocity=0,Motion_Control=0, Equation=0):
+    def MaterialHandling(self,Bool_Mat=False, Module=0,Function=0,Height=0,Bed_Selection=0,Motor_Velocity=0,Motion_Control=0, Equation=0):
         root = self.Cluster("Material Handling",4)
         root.append(self.Boolean("Material Handling",Bool_Mat))
 
@@ -557,7 +555,7 @@ class gcodeWriterVlaseaBM(gcodeWriter):
 
         return root
     
-    def ZAxis(self,Bool_ZAxis = 0 , Module = 0, Function = 0, Z_Coord = 0, Motor_Selection = 0, Motor_Velocity = 0):
+    def ZAxis(self,Bool_ZAxis = False , Module = 0, Function = 0, Z_Coord = 0, Motor_Selection = 0, Motor_Velocity = 0):
 
         root = self.Cluster("Z Axis",4)
         root.append(self.Boolean("Z Axis",Bool_ZAxis))
