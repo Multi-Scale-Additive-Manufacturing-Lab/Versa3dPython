4a4984ccecb68f344ed2f979f96bf149244afa10
rockandsalt <marcwang17@hotmail.com>
2018-07-24 17:48:23 -0400

    moved shelling function to it's own standalone function

--- src/lib/slicing.py
+++ src/lib/slicing.py
@@ -36,6 +36,90 @@ def findDistanceToLine(Point, line):
     u = line[1]
     return abs(np.linalg.norm(np.cross(AP,u))/np.linalg.norm(u))
 
+def offsetContour(contour,thickness):
+    """Shelling algorithm, only works with continuous contour
+    
+    Arguments:
+        contour {vtkPolyData} -- vtkPolyData containing lines
+        thickness {float} -- set thickness of shell
+    """
+
+    if(contour.GetNumberOfLines() != 0):
+        LineIterator = contour.GetLines()
+        LineIterator.InitTraversal()
+
+        idList = vtk.vtkIdList()
+
+        VertexState = [False]*contour.GetNumberOfPoints()
+        
+        SLAV = []
+        LAV = []
+        InterceptQueue = q.PriorityQueue()
+
+        while(LineIterator.GetNextCell(idList)):
+            numID = idList.GetNumberOfIds()
+            LAV = [idList.GetId(i)for i in range(numID)]
+            if(LAV[0] == LAV[-1]):
+                LAV.pop()
+            SLAV.append(LAV)
+        
+        for LAV in SLAV:
+            length = len(LAV)
+            listofbisector = []
+            #compute all bisector
+            for i in range(length):
+                PrevVertexId = LAV[(i-1)%length]
+                VertexId = LAV[i]
+                NextVertexId = LAV[(i+1)%length]
+
+                PrevVertex = np.array(contour.GetPoint(PrevVertexId))
+                Vertex = np.array(contour.GetPoint(VertexId))
+                NextVertex = np.array(contour.GetPoint(NextVertexId))
+
+                edge1 = Vertex - PrevVertex
+                edge2 = NextVertex - Vertex
+                
+                ratio = np.linalg.norm(edge1)/np.linalg.norm(edge2)
+
+                edge3 = (PrevVertex - NextVertex)
+
+                bisector = ratio/(1+ratio)*edge3+PrevVertex
+
+                normalizedBisector = bisector/np.linalg.norm(bisector)
+
+                listofbisector.append(normalizedBisector)
+            
+            #Compute intercept
+            for i in range(length):
+                PrevBisector = listofbisector[(i-1)%length]
+                Bisector = listofbisector[i]
+                NextBisector = listofbisector[(i+1)%length]
+
+                PrevVertex = np.array(contour.GetPoint(LAV[(i-1)%length]))
+                Vertex = np.array(contour.GetPoint(LAV[i]))
+                NextVertex = np.array(contour.GetPoint(LAV[(i+1)%length]))
+
+                if(np.dot(Bisector,prevBisector) != 1):
+                    I1 = findInterceptPoint(( PrevVertex, PrevBisector),(Vertex, Bisector))
+                    I2 = findInterceptPoint(( NextVertex, NextBisector),(Vertex, Bisector))
+                    
+                    edge = NextVertex - Vertex
+
+                    distToEdge1 = findDistanceToLine(I1,(Vertex,edge))
+                    distToEdge2 = findDistanceToLine(I2,(Vertex,edge))
+                    if(distToEdge1 > distToEdge2):
+                        InterceptQueue.put((distToEdge1,[I1,PrevVertexId,VertexId]))
+                    else:
+                        InterceptQueue.put((distToEdge2,[I2,VertexId,NextVertexId]))
+        
+            while(not InterceptQueue.empty()):
+                distToEdge, cluster = InterceptQueue.get()
+                I, VaId,VbId = cluster
+                if(not VertexState[VaId] and not VertexState[VbId]):
+                    pass
+                PrevVertex = np.array(contour.GetPoint(VaId))
+
+
 class slice():
     def __init__(self, height,thickess):
         self._height = height
@@ -232,91 +316,3 @@ class CheckerBoardImageSlicer(VoxelSlicer):
         bound = mergedPoly.GetBounds()
 
         listOfContour = self._slicePoly(bound[4:6],self._thickness,mergedPoly)
-
-        for contour in listOfContour:
-            self.offsetContour(contour,0.1)
-    
-    def offsetContour(self,contour,thickness):
-        """Shelling algorithm, only works with continuous contour
-        
-        Arguments:
-            contour {vtkPolyData} -- vtkPolyData containing lines
-            thickness {float} -- set thickness of shell
-        """
-
-        if(contour.GetNumberOfLines() != 0):
-            LineIterator = contour.GetLines()
-            LineIterator.InitTraversal()
-
-            idList = vtk.vtkIdList()
-
-            VertexState = [False]*contour.GetNumberOfPoints()
-            
-            SLAV = []
-            LAV = []
-            InterceptQueue = q.PriorityQueue()
-
-            while(LineIterator.GetNextCell(idList)):
-                numID = idList.GetNumberOfIds()
-                LAV = [idList.GetId(i)for i in range(numID)]
-                if(LAV[0] == LAV[-1]):
-                    LAV.pop()
-                SLAV.append(LAV)
-            
-            for LAV in SLAV:
-                length = len(LAV)
-                listofbisector = []
-                #compute all bisector
-                for i in range(length):
-                    PrevVertexId = LAV[(i-1)%length]
-                    VertexId = LAV[i]
-                    NextVertexId = LAV[(i+1)%length]
-
-                    PrevVertex = np.array(contour.GetPoint(PrevVertexId))
-                    Vertex = np.array(contour.GetPoint(VertexId))
-                    NextVertex = np.array(contour.GetPoint(NextVertexId))
-
-                    edge1 = Vertex - PrevVertex
-                    edge2 = NextVertex - Vertex
-                    
-                    ratio = np.linalg.norm(edge1)/np.linalg.norm(edge2)
-
-                    edge3 = (PrevVertex - NextVertex)
-
-                    bisector = ratio/(1+ratio)*edge3+PrevVertex
-
-                    normalizedBisector = bisector/np.linalg.norm(bisector)
-
-                    listofbisector.append(normalizedBisector)
-                
-                #Compute intercept
-                for i in range(length):
-                    PrevBisector = listofbisector[(i-1)%length]
-                    Bisector = listofbisector[i]
-                    NextBisector = listofbisector[(i+1)%length]
-
-                    PrevVertex = np.array(contour.GetPoint(LAV[(i-1)%length]))
-                    Vertex = np.array(contour.GetPoint(LAV[i]))
-                    NextVertex = np.array(contour.GetPoint(LAV[(i+1)%length]))
-
-                    if(np.dot(Bisector,prevBisector) != 1):
-                        I1 = findInterceptPoint(( PrevVertex, PrevBisector),(Vertex, Bisector))
-                        I2 = findInterceptPoint(( NextVertex, NextBisector),(Vertex, Bisector))
-                        
-                        edge = NextVertex - Vertex
-
-                        distToEdge1 = findDistanceToLine(I1,(Vertex,edge))
-                        distToEdge2 = findDistanceToLine(I2,(Vertex,edge))
-                        if(distToEdge1 > distToEdge2):
-                            InterceptQueue.put((distToEdge1,[I1,PrevVertexId,VertexId]))
-                        else:
-                            InterceptQueue.put((distToEdge2,[I2,VertexId,NextVertexId]))
-            
-                while(not InterceptQueue.empty()):
-                    distToEdge, cluster = InterceptQueue.get()
-                    I, VaId,VbId = cluster
-                    if(not VertexState[VaId] and not VertexState[VbId]):
-                        pass
-                    PrevVertex = np.array(contour.GetPoint(VaId))
-
-                #print("vertex:{}, i: {}, id:{} \n".format(vertex,i,id))
