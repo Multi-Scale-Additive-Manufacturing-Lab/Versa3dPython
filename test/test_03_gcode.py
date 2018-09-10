f19e6e3b3a1fe28c6c59ea411f6a6a8c85e69296
rockandsalt <marcwang17@hotmail.com>
2018-09-10 13:36:00 -0400

    output checkerboard gcode

--- test/test_03_gcode.py
+++ test/test_03_gcode.py
@@ -2,24 +2,19 @@ import unittest
 import os
 from src.lib.versa3dConfig import config
 from src.lib.gcode import gcodeWriterVlaseaBM
-from src.lib.slicing import slicerFactory,FullBlackImageSlicer,VoxelSlicer
+import src.lib.slicing as sl
 from lxml import etree
 import shutil
 import vtk
+
 
 class gcodeTest(unittest.TestCase):
 
     def setUp(self):
         self.testConfigFolder = './configtest'
-        self.OutputFolder = './test/testOutput/Gcode'
+        
         if(not os.path.isdir('./test/testOutput')):
             os.mkdir('./test/testOutput')
-        
-        if(not os.path.isdir(self.OutputFolder)):
-            os.mkdir(self.OutputFolder)
-        else:
-            shutil.rmtree(self.OutputFolder)
-            os.mkdir(self.OutputFolder)
 
         os.mkdir(self.testConfigFolder)
         self.test_config = config(self.testConfigFolder)
@@ -45,24 +40,48 @@ class gcodeTest(unittest.TestCase):
         newPosition[0] = printBedSize[0]/2
         newPosition[1] = printBedSize[1]/2
 
-        if(zRange[0]<0):
+        if(zRange[0] < 0):
             newPosition[2] = oldPosition[2]-zRange[0]
         else:
             newPosition[2] = oldPosition[2]
-        
+
         self.stlActor.SetPosition(newPosition)
-        self.stlActor.RotateZ(45) 
-    
+        self.stlActor.RotateZ(45)
+
     def test_generategcode(self):
-        
-        blackSlicer = FullBlackImageSlicer(self.test_config)
+        output_folder_black = './test/testOutput/Gcode'
+
+        if(not os.path.isdir(output_folder_black)):
+            os.mkdir(output_folder_black)
+        else:
+            shutil.rmtree(output_folder_black)
+            os.mkdir(output_folder_black)
+
+        blackSlicer = sl.FullBlackImageSlicer(self.test_config)
         blackSlicer.addActor(self.stlActor)
         BuildVtkImage = blackSlicer.slice()
 
-        gcodewriter = gcodeWriterVlaseaBM(self.test_config,self.OutputFolder)
+        gcodewriter = gcodeWriterVlaseaBM(self.test_config, output_folder_black)
         gcodewriter.SetInput(blackSlicer)
         gcodewriter.generateGCode()
-        
+
+    def test_generate_gcode_checkerboard(self):
+
+        output_folder_check = './test/testOutput/GcodeCheck'
+
+        if(not os.path.isdir(output_folder_check)):
+            os.mkdir(output_folder_check)
+        else:
+            shutil.rmtree(output_folder_check)
+            os.mkdir(output_folder_check)
+
+        checker_board_slicer = sl.CheckerBoardImageSlicer(self.test_config)
+        checker_board_slicer.addActor(self.stlActor)
+        checker_board_slicer.slice()
+
+        gcodewriter = gcodeWriterVlaseaBM(self.test_config, output_folder_check)
+        gcodewriter.SetInput(checker_board_slicer)
+        gcodewriter.generateGCode()
 
     def tearDown(self):
         shutil.rmtree(self.testConfigFolder)
