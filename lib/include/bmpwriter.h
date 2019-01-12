8d9e6946aab159f5c5d5d0fbc736250d42f0a074
rockandsalt <marcwang17@hotmail.com>
2019-01-11 23:50:00 -0500

    formatted doc

--- lib/include/bmpwriter.h
+++ lib/include/bmpwriter.h
@@ -8,22 +8,22 @@
 
 using namespace std;
 
-
 class bmpwriter
 {
-    private:
-        map<int,vector<vector<float>>> diffusion_map;
-        vtkNew<vtkImageData> data;
-        const char *file_path;
-        float dither(int i,int j);
-        int dither_map;
-        float find_closest_color(float val);
-        void propagate_error(int i,int j,float error);
+  private:
+    map<int, vector<vector<float>>> diffusion_map;
+    vtkNew<vtkImageData> data;
+    const char *file_path;
+    float dither(int i, int j);
+    int dither_map;
+    float find_closest_color(float val);
+    void propagate_error(int i, int j, float error);
 
-    public:
-        bmpwriter(const char *file_path, vtkImageData *img);
-        void set_dither_map(int map);
-        void write_to_file();
+  public:
+    bmpwriter(const char *file_path, vtkImageData *img);
+    void set_dither_map(int map);
+    void write_to_file();
+    float *split_print();
 };
 
 #endif
