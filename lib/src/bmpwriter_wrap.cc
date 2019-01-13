b19e668f9a821d742789c0671edb4882a8e83880
rockandsalt <marcwang17@hotmail.com>
2019-01-13 13:54:18 -0500

    Update bmpwriter_wrap.cc
    added doc string

--- lib/src/bmpwriter_wrap.cc
+++ lib/src/bmpwriter_wrap.cc
@@ -9,9 +9,14 @@ PYBIND11_VTK_TYPECASTER(vtkImageData)
 
 PYBIND11_MODULE(Versa3dLib, m)
 {
-	py::class_<bmpwriter>(m,"bmpwriter")
+	py::class_<bmpwriter>(m, "bmpwriter")
 		.def(py::init<vtkImageData *>())
-		.def("write_to_file",&bmpwriter::write_to_file)
-		.def("split_print",&bmpwriter::split_print)
-	;
+		.def("write_to_file", &bmpwriter::write_to_file,
+			 "write vtkImgData to monochrome bmp",
+			 py::arg("file_path"))
+		.def("split_print", &bmpwriter::split_print,
+			 "write vtkImgData to monochrome BMP in imtech compatible size",
+			 py::arg("file_path"),
+			 py::arg("margin"),
+			 py::arg("size_limit"));
 }
