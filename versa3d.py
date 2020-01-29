63b5a5f9eda0d724af7a91c67cd99b761f12ff4c
rockandsalt <marcwang17@hotmail.com>
2020-01-29 16:11:15 -0500

    Create versa3d.py
    renamed main

---
+++ versa3d.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 11:47:41 2018
+
+@author: inst
+"""
+
+import sys
+from os import path
+from PyQt5 import QtWidgets
+from src.mainWindow import MainWindow
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    
+    window = MainWindow(path.join('GUI','Versa3dMainWindow.ui'))
+    window.show()
+    
+    sys.exit(app.exec_())
