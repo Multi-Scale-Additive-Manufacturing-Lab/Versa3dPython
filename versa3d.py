a21156996be5d1dd393d9fc1c9c378576e8d6da3
rockandsalt <marcwang17@hotmail.com>
2020-01-29 16:37:24 -0500

    Update versa3d.py
    pyinstaller is now path aware

--- versa3d.py
+++ versa3d.py
@@ -6,14 +6,23 @@ Created on Mon Feb 26 11:47:41 2018
 """
 
 import sys
-from os import path
+import os
 from PyQt5 import QtWidgets
 from src.mainWindow import MainWindow
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
-    window = MainWindow(path.join('GUI', 'Versa3dMainWindow.ui'))
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+        ui_path = os.path.join(base_path, 'GUI', 'Versa3dMainWindow.ui')
+    except Exception:
+        base_path = os.path.abspath(".")
+        ui_path = os.path.join(
+            base_path, 'designer_files', 'Versa3dMainWindow.ui')
+
+    window = MainWindow(ui_path)
     window.show()
 
     sys.exit(app.exec_())
