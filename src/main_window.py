b22dd7985bc03229bb00e7db77481cccd669a430
rockandsalt <marcwang17@hotmail.com>
2020-03-03 10:23:15 -0500

    moved setup scene to print platter

--- src/main_window.py
+++ src/main_window.py
@@ -3,7 +3,6 @@
 from PyQt5 import QtWidgets, uic
 import numpy as np
 import vtk
-from vtk.util import numpy_support
 from src.versa3d_settings import load_settings, save_settings
 from src.mouse_interaction import actor_highlight
 import src.print_platter as ppl
@@ -27,7 +26,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.stl_interactor = self.ui.vtkWidget.GetRenderWindow().GetInteractor()
         self.platter = ppl.print_platter(self.stl_renderer)
-        
+
         style = vtk.vtkInteractorStyleRubberBand3D()
         self.stl_interactor.SetInteractorStyle(style)
 
@@ -39,61 +38,10 @@ class MainWindow(QtWidgets.QMainWindow):
         y_sc = self.settings.value('basic_printer/bed_y', 50, type=float)
         z_sc = self.settings.value('basic_printer/bed_z', 100, type=float)
 
-        self.setup_scene((x_sc, y_sc, z_sc))
+        self.platter.setup_scene((x_sc, y_sc, z_sc))
         self.platter.set_up_dummy_sphere()
 
         self.stl_interactor.Initialize()
 
         self.undo_stack = QtWidgets.QUndoStack(self)
         self.undo_stack.setUndoLimit(10)
-
-    def setup_scene(self, size):
-        """set grid scene
-
-        Arguments:
-            size {array(3,)} -- size of scene
-        """
-        colors = vtk.vtkNamedColors()
-
-        self.stl_renderer.SetBackground(colors.GetColor3d("Gray"))
-
-        # add coordinate axis
-        axes_actor = vtk.vtkAxesActor()
-        axes_actor.SetShaftTypeToLine()
-        axes_actor.SetTipTypeToCone()
-
-        axes_actor.SetTotalLength(*size)
-
-        number_grid = 50
-
-        X = numpy_support.numpy_to_vtk(np.linspace(0, size[0], number_grid))
-        Y = numpy_support.numpy_to_vtk(np.linspace(0, size[1], number_grid))
-        Z = numpy_support.numpy_to_vtk(np.array([0]*number_grid))
-
-        # set up grid
-        grid = vtk.vtkRectilinearGrid()
-        grid.SetDimensions(number_grid, number_grid, number_grid)
-        grid.SetXCoordinates(X)
-        grid.SetYCoordinates(Y)
-        grid.SetZCoordinates(Z)
-
-        geometry_filter = vtk.vtkRectilinearGridGeometryFilter()
-        geometry_filter.SetInputData(grid)
-        geometry_filter.SetExtent(
-            0, number_grid - 1, 0, number_grid - 1, 0, number_grid - 1)
-        geometry_filter.Update()
-
-        grid_mapper = vtk.vtkPolyDataMapper()
-        grid_mapper.SetInputConnection(geometry_filter.GetOutputPort())
-
-        grid_actor = vtk.vtkActor()
-        grid_actor.SetMapper(grid_mapper)
-        grid_actor.GetProperty().SetRepresentationToWireframe()
-        grid_actor.GetProperty().SetColor(colors.GetColor3d('Banana'))
-        grid_actor.GetProperty().EdgeVisibilityOn()
-        grid_actor.SetPickable(False)
-        grid_actor.SetDragable(False)
-
-        self.stl_renderer.AddActor(axes_actor)
-        self.stl_renderer.AddActor(grid_actor)
-        self.stl_renderer.ResetCamera()
--- src/print_platter.py
+++ src/print_platter.py
@@ -1,4 +1,7 @@
 import vtk
+from vtk.util import numpy_support
+import numpy as np
+
 
 class print_object():
     def __init__(self, vtk_obj):
@@ -6,7 +9,7 @@ class print_object():
         self._vtkactor = vtk_obj
         self._picked_state = False
         self._backup_prop = None
-    
+
     @property
     def actor(self):
         return self._vtkactor
@@ -31,8 +34,9 @@ class print_object():
     def unpick(self):
         if(self._picked_state and (self._backup_prop is not None)):
             self._vtkactor.GetProperty().DeepCopy(self._backup_prop)
-        
+
         self._picked_state = False
+
 
 class print_platter():
 
@@ -41,7 +45,7 @@ class print_platter():
 
         self._parts = []
         self._renderer = renderer
-    
+
     @property
     def platter(self):
         return self._parts
@@ -49,12 +53,12 @@ class print_platter():
     def add_parts(self, part):
         self._parts.append(part)
         self._renderer.AddActor(part.actor)
-    
+
     def reset_picked(self):
         for part in self._parts:
             if(part.picked):
                 part.unpick()
-    
+
     def set_up_dummy_sphere(self):
         for i in range(5):
             source = vtk.vtkSphereSource()
@@ -87,3 +91,54 @@ class print_platter():
             print_obj = print_object(actor)
             actor.AddObserver('PickEvent', print_obj.pick)
             self.add_parts(print_obj)
+
+    def setup_scene(self, size):
+        """set grid scene
+
+        Arguments:
+            size {array(3,)} -- size of scene
+        """
+        colors = vtk.vtkNamedColors()
+
+        self._renderer.SetBackground(colors.GetColor3d("Gray"))
+
+        # add coordinate axis
+        axes_actor = vtk.vtkAxesActor()
+        axes_actor.SetShaftTypeToLine()
+        axes_actor.SetTipTypeToCone()
+
+        axes_actor.SetTotalLength(*size)
+
+        number_grid = 50
+
+        X = numpy_support.numpy_to_vtk(np.linspace(0, size[0], number_grid))
+        Y = numpy_support.numpy_to_vtk(np.linspace(0, size[1], number_grid))
+        Z = numpy_support.numpy_to_vtk(np.array([0]*number_grid))
+
+        # set up grid
+        grid = vtk.vtkRectilinearGrid()
+        grid.SetDimensions(number_grid, number_grid, number_grid)
+        grid.SetXCoordinates(X)
+        grid.SetYCoordinates(Y)
+        grid.SetZCoordinates(Z)
+
+        geometry_filter = vtk.vtkRectilinearGridGeometryFilter()
+        geometry_filter.SetInputData(grid)
+        geometry_filter.SetExtent(
+            0, number_grid - 1, 0, number_grid - 1, 0, number_grid - 1)
+        geometry_filter.Update()
+
+        grid_mapper = vtk.vtkPolyDataMapper()
+        grid_mapper.SetInputConnection(geometry_filter.GetOutputPort())
+
+        grid_actor = vtk.vtkActor()
+        grid_actor.SetMapper(grid_mapper)
+        grid_actor.GetProperty().SetRepresentationToWireframe()
+        grid_actor.GetProperty().SetColor(colors.GetColor3d('Banana'))
+        grid_actor.GetProperty().EdgeVisibilityOn()
+        grid_actor.SetPickable(False)
+        grid_actor.SetDragable(False)
+
+        self._renderer.AddActor(axes_actor)
+        self._renderer.AddActor(grid_actor)
+        self._renderer.ResetCamera()
