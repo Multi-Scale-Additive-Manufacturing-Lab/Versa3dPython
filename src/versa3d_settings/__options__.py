af3fd7c471288c9fae30641db2f33b77c42b12b7
mwang <marcwang17@hotmail.com>
2020-03-10 23:04:38 -0400

    Update __options__.py
    removed multiple class for single option

--- src/versa3d_settings/__options__.py
+++ src/versa3d_settings/__options__.py
@@ -23,20 +23,8 @@ class versa3d_single_option():
     def value(self, val):
         self._value = val
 
-
-class versa3d_float_option(versa3d_single_option):
-    def __init__(self, value):
-        super().__init__(self, value)
-
-    @pyqtSlot(float)
-    def update_value(self, value):
-        self._value = value
-
-
-class versa3d_int_option(versa3d_single_option):
-    def __init__(self, value):
-        super().__init__(self, value)
-
     @pyqtSlot(int)
+    @pyqtSlot(float)
+    @pyqtSlot(str)
     def update_value(self, value):
         self._value = value
