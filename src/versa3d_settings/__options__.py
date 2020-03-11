9295d30dc4f12dde5a43904956a6230a2964eecb
mwang <marcwang17@hotmail.com>
2020-03-10 22:23:42 -0400

    Update __options__.py
    added more option

--- src/versa3d_settings/__options__.py
+++ src/versa3d_settings/__options__.py
@@ -1,9 +1,11 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import pyqtSlot
+
 
 class versa3d_single_option():
-    def __init__(self):
+    def __init__(self, default_value):
         super().__init__()
-        self._value = None
+        self._value = default_value
 
         self.label = ""
         self.tooltip = ""
@@ -11,7 +13,7 @@ class versa3d_single_option():
         self.category = ""
         self.subcategory = ""
 
-        self.default_value = None
+        self._default_value = default_value
 
     @property
     def value(self):
@@ -20,3 +22,21 @@ class versa3d_single_option():
     @value.setter
     def value(self, val):
         self._value = val
+
+
+class versa3d_float_option(versa3d_single_option):
+    def __init__(self, value):
+        super().__init__(self, value)
+
+    @pyqtSlot(float)
+    def update_value(self, value):
+        self._value = value
+
+
+class versa3d_int_option(versa3d_single_option):
+    def __init__(self, value):
+        super().__init__(self, value)
+
+    @pyqtSlot(int)
+    def update_value(self, value):
+        self._value = value
