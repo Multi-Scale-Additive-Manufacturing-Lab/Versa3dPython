f2065c7124941ce8f22ecc0bb9e411536bbafdd6
mwang <marcwang17@hotmail.com>
2020-03-14 16:05:59 -0400

    Update versa3d.py
    change name

--- versa3d.py
+++ versa3d.py
@@ -8,7 +8,7 @@ Created on Mon Feb 26 11:47:41 2018
 import sys
 import os
 from PyQt5 import QtWidgets
-from src.main_window import MainWindow
+from versa3d.main_window import MainWindow
 
 if __name__ == "__main__":
     APP = QtWidgets.QApplication(sys.argv)
