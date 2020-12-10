9cc2e0e8c4252073ac2ca52b34caebf36e04b57b
Marc Wang <m384wang@uwaterloo.ca>
2020-12-09 22:06:13 -0500

    cloning done
    cloning settings is done

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,7 +1,7 @@
 import unittest
 from PyQt5.QtCore import QSettings, QCoreApplication
 
-from versa3d.settings import Versa3dSettings, DEFAULT_CONFIG
+from versa3d.settings import Versa3dSettings, PRINTER_CONFIG, PRESET_PARAM_CONFIG
 import json
 import numpy as np
 
@@ -16,40 +16,36 @@ class SettingsTest(unittest.TestCase):
         settings.clear()
 
         self.singleton = Versa3dSettings()
-        self.singleton.init_default()
-        self.singleton.load_all()
 
     def test_setting_init(self):
-        with open(DEFAULT_CONFIG) as f:
-            default_config = json.load(f)
+        with open(PRINTER_CONFIG) as f:
+            printer_default_config = json.load(f)
+        
+        with open(PRESET_PARAM_CONFIG) as f:
+            preset_default_config = json.load(f)
 
         saved_coord_offset = np.array(
-            default_config['binder_jetting_printer']['coord_offset']['value'])
-        self.assertTrue(np.all(saved_coord_offset ==
-                               self.singleton.get_printer('default_bj_printer').coord_offset))
+            printer_default_config['bj_printer']['coord_offset']['value'])
+        stored_coord_offset = self.singleton.get_printer('bj_printer')['coord_offset'].value
+        self.assertTrue(np.all(saved_coord_offset == stored_coord_offset))
+        self.assertEqual(self.singleton.get_printer('bj_printer').process_type ,'bjp')
 
-        saved_value = default_config['binder_jetting_parameter_preset']['layer_thickness']['value']
-        self.assertEqual(
-            saved_value, self.singleton.get_preset('defaut_bj_parameter').layer_thickness)
+        saved_value = preset_default_config['bj_parameter']['layer_thickness']['value']
+        stored_value = self.singleton.get_preset('bj_parameter')['layer_thickness'].value
+        self.assertEqual(saved_value, stored_value)
 
     def test_set_settings(self):
-        test_array = np.array([100.0, 100.0])
-        default_name = 'default_bj_printer'
-        modified_printer_name = 'modified_printer_10'
-        self.singleton.clone_printer(default_name, modified_printer_name)
-        self.singleton.update_printer_value(modified_printer_name, 'coord_offset', test_array)
-        self.singleton.save_to_disk('printer', modified_printer_name)
+        modified_coord = np.array([10,10], dtype = float)
+        self.singleton.clone_printer('bj_printer', 'new_printer')
+        old_val = self.singleton.get_printer('new_printer')['coord_offset'].value
+        self.singleton.get_printer('new_printer')['coord_offset'].value = modified_coord
+        self.singleton.save_printer('new_printer')
 
         singleton_2 = Versa3dSettings()
-        singleton_2.load_all()
-        default_val = singleton_2.get_printer(default_name).coord_offset
-        self.assertFalse(
-            np.all(default_val == test_array)
-        )
-        changed_val = singleton_2.get_printer(modified_printer_name).coord_offset
-        self.assertTrue(
-            np.all(changed_val == test_array)
-        )
+        new_coord = singleton_2.get_printer('new_printer')['coord_offset'].value
+
+        self.assertTrue(np.all(new_coord == modified_coord))
+        self.assertFalse(np.all(new_coord == old_val))
 
     def tearDown(self):
         settings = QSettings()
