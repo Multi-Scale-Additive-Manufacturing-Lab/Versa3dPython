893f4104c89522d756e85768918aa72f93595def
marc wang <marc.wang@uwaterloo.ca>
2021-01-22 16:33:01 -0500

    Update mouse_interaction.py
    implemented as switch interactor

--- versa3d/mouse_interaction.py
+++ versa3d/mouse_interaction.py
@@ -1,22 +1,20 @@
 import vtk
 from PyQt5.QtWidgets import QUndoCommand
 
-from vtkmodules.vtkInteractionStyle import vtkInteractorStyleRubberBand3D
+from vtkmodules.vtkInteractionStyle import vtkInteractorStyleSwitch, vtkInteractorStyleTrackballCamera, vtkInteractorStyleTrackballActor
 from vtkmodules.vtkRenderingCore import vtkCellPicker, vtkAssembly
 from versa3d.controller import Versa3dController
 from enum import IntEnum
 
 
-class RubberBandHighlight(vtkInteractorStyleRubberBand3D):
+class RubberBandHighlight(vtkInteractorStyleSwitch):
 
     def __init__(self) -> None:
         super().__init__()
-
-        self.actor_mode = False
-
-        self.AddObserver('SelectionChangedEvent', highlight)
-        self.AddObserver('LeftButtonPressEvent', move_actor)
-        self.picker = vtk.vtkRenderedAreaPicker()
+        self.SetCurrentStyleToTrackballCamera()
+        track_ball_cam = self.GetCurrentStyle()
+        track_ball_cam.AddObserver('LeftButtonPressEvent', self.highlight)
+        #self.AddObserver('LeftButtonPressEvent', move_actor)
         self.selected_actor = vtkAssembly()
 
     def reset(self):
@@ -31,44 +29,23 @@ class RubberBandHighlight(vtkInteractorStyleRubberBand3D):
 
             self.selected_actor = vtkAssembly()
 
+    def highlight(self, obj: vtkInteractorStyleTrackballCamera, event: str):
 
-def move_actor(obj: RubberBandHighlight, event: str):
-    interactor = obj.GetInteractor()
-    x = interactor.GetEventPosition()[0]
-    y = interactor.GetEventPosition()[1]
-    ray_picker = vtkCellPicker()
-    ray_picker.SetTolerance(0.001)
-    ren = interactor.FindPokedRenderer(x, y)
+        interactor = obj.GetInteractor()
+        if not interactor.GetShiftKey():
+            self.reset()
 
-    ray_picker.Pick(x, y, 0.0, ren)
+        x = interactor.GetEventPosition()[0]
+        y = interactor.GetEventPosition()[1]
+        picker = vtkCellPicker()
+        ren = interactor.FindPokedRenderer(x, y)
+        picker.Pick(x, y, 0, ren)
 
-    picked_actor = ray_picker.GetViewProp()
-    if picked_actor != None:
-        is_selected = obj.selected_actor.GetParts().IsItemPresent(picked_actor)
-        obj.actor_mode = True
-    else:
+        prop = picker.GetViewProp()
+        if not prop is None:
+            prop.Pick()
+            self.selected_actor.AddPart(prop)
+
+        ren.GetRenderWindow().Render()
+
         obj.OnLeftButtonDown()
-
-
-def highlight(obj: RubberBandHighlight, event: str):
-    obj.actor_mode = False
-    interactor = obj.GetInteractor()
-    if not interactor.GetShiftKey():
-        obj.reset()
-
-    start_pos = obj.GetStartPosition()
-    end_pos = obj.GetEndPosition()
-    ren = interactor.FindPokedRenderer(start_pos[0], start_pos[1])
-
-    obj.picker.AreaPick(start_pos[0], start_pos[1],
-                        end_pos[0], end_pos[1], ren)
-
-    list_actors = obj.picker.GetProp3Ds()
-    num_picked_actor = list_actors.GetNumberOfItems()
-
-    for i in range(num_picked_actor):
-        actor = list_actors.GetItemAsObject(i)
-        actor.Pick()
-        obj.selected_actor.AddPart(actor)
-
-    ren.GetRenderWindow().Render()
