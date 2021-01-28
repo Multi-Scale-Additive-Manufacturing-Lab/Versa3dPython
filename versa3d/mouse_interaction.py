aa1b242acb57d1da2c8b1146b24107f56b8b6611
marc wang <marc.wang@uwaterloo.ca>
2021-01-27 20:29:27 -0500

    mouse interaction works
    finally

--- versa3d/mouse_interaction.py
+++ versa3d/mouse_interaction.py
@@ -1,81 +1,48 @@
-import vtk
-from PyQt5.QtWidgets import QUndoCommand
-
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleRubberBand3D
-from vtkmodules.vtkRenderingCore import vtkCellPicker, vtkAssembly, vtkInteractorStyle
+from vtkmodules.vtkRenderingCore import vtkCellPicker, vtkAssembly, vtkInteractorStyle, vtkProp3DCollection, vtkRenderedAreaPicker
 from vtkmodules.vtkInteractionWidgets import vtkBoxWidget2, vtkBoxRepresentation
-from versa3d.controller import Versa3dController
-from enum import IntEnum
+from vtkmodules.vtkCommonCore import vtkInformation, vtkInformationVector
 
-import numpy as np
 
 class RubberBandHighlight(vtkInteractorStyleRubberBand3D):
 
     def __init__(self) -> None:
         super().__init__()
-        #track_ball_camera.AddObserver('LeftButtonPressEvent', self.highlight)
-        #track_ball_camera.AddObserver('LeftButtonPressEvent', self.switch_mode)
-        self.selected_actor = vtkAssembly()
+        self.selected_actor = []
         self.AddObserver('SelectionChangedEvent', self.highlight)
 
-        self.box_widget = vtkBoxWidget2()
-        self.box_widget.TranslationEnabledOn()
-        self.box_widget.RotationEnabledOn()
-        self.box_widget.ScalingEnabledOff()
-        self.box_widget.AddObserver('InteractionEvent', self.box_cb)
-        self.PickingManagedOn()
-
-    def reset(self) -> None:
-        actor_it = self.selected_actor.GetParts()
-        if actor_it.GetNumberOfItems() > 0:
-            self.selected_actor = vtkAssembly()
-            self.box_widget.Off()
-    
-    def find_poked_actor(self, style : vtkInteractorStyle) -> vtk.vtkProp3DCollection:
+    def find_poked_actor(self, style: vtkInteractorStyleRubberBand3D) -> vtkProp3DCollection:
         interactor = style.GetInteractor()
 
-        picker = vtk.vtkRenderedAreaPicker()
+        picker = vtkRenderedAreaPicker()
         start_pos = style.GetStartPosition()
         end_pos = style.GetEndPosition()
         ren = interactor.FindPokedRenderer(start_pos[0], start_pos[1])
-        picker.AreaPick(start_pos[0], start_pos[1],end_pos[0], end_pos[1], ren)
+        picker.AreaPick(start_pos[0], start_pos[1],
+                        end_pos[0], end_pos[1], ren)
         props = picker.GetProp3Ds()
         return props
-    
-    def update_render(self):
+
+    def update_render(self) -> None:
         interactor = self.GetInteractor()
         x = interactor.GetEventPosition()[0]
         y = interactor.GetEventPosition()[1]
         ren = interactor.FindPokedRenderer(x, y)
         ren.GetRenderWindow().Render()
-    
+
     def highlight(self, obj: vtkInteractorStyleRubberBand3D, event: str) -> None:
         interactor = obj.GetInteractor()
-        self.box_widget.SetInteractor(interactor)
-        if not interactor.GetShiftKey():
-            self.reset()
         prop_collection = self.find_poked_actor(obj)
         prop_collection.InitTraversal()
         if prop_collection.GetNumberOfItems() > 0:
             prop = prop_collection.GetNextProp()
             while not prop is None:
-                self.selected_actor.AddPart(prop)
+                self.selected_actor.append(prop)
+                prop.InvokeEvent('StartPickEvent', interactor)
                 prop = prop_collection.GetNextProp()
+        else:
+            for prop in self.selected_actor:
+                prop.InvokeEvent('EndPickEvent', interactor)
+            self.selected_actor = []
 
-            box_rep = vtkBoxRepresentation()
-            box_rep.PickingManagedOn()
-            box_rep.SetPlaceFactor(1.50)
-            box_rep.PlaceWidget(self.selected_actor.GetBounds())
-            self.box_widget.SetRepresentation(box_rep)
-            self.box_widget.On()
-    
-    def box_cb(self, obj : vtkBoxWidget2, event : str):
-        t = vtk.vtkTransform()
-        obj.GetRepresentation().GetTransform(t)
-
-        actor_it = self.selected_actor.GetParts()
-        actor_it.InitTraversal()
-        prop = actor_it.GetNextProp()
-        while not prop is None:
-            prop.SetUserTransform(t)
-            prop = actor_it.GetNextProp()
+        self.update_render()
--- versa3d/print_platter.py
+++ versa3d/print_platter.py
@@ -1,64 +1,80 @@
-import vtk
 import numpy as np
-from vtk.util.vtkAlgorithm import VTKPythonAlgorithmBase
+from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
 from vtkmodules.vtkInteractionWidgets import vtkBoxWidget
+from vtkmodules.vtkCommonCore import vtkInformation, vtkInformationVector
+from vtkmodules.vtkCommonDataModel import vtkPolyData
+from vtkmodules.util.vtkConstants import VTK_OBJECT
+from vtkmodules.vtkCommonTransforms import vtkTransform
+from vtkmodules.vtkRenderingCore import vtkRenderWindowInteractor, vtkActor, vtkRenderer, vtkPolyDataMapper
+from vtkmodules.vtkFiltersGeneral import vtkTransformPolyDataFilter
+from vtkmodules.util.misc import calldata_type
 from PyQt5.QtCore import QUuid
-
 
 class PrintObject(VTKPythonAlgorithmBase):
     def __init__(self) -> None:
         VTKPythonAlgorithmBase.__init__(
             self, nInputPorts=1, inputType='vtkPolyData', nOutputPorts=1, outputType='vtkPolyData')
 
-        self._actor = vtk.vtkActor()
+        self._actor = vtkActor()
+        self._actor.AddObserver('StartPickEvent', self.pick)
+        self._actor.AddObserver('EndPickEvent', self.unpick)
+
+        self._box_widget = vtkBoxWidget()
+        self._box_widget.TranslationEnabledOn()
+        self._box_widget.RotationEnabledOn()
+        self._box_widget.ScalingEnabledOff()
+        self._box_widget.SetProp3D(self._actor)
+        self._box_widget.PickingManagedOn()
+        self._box_widget.SetPlaceFactor(1.50)
+        self._box_widget.AddObserver('InteractionEvent', self.box_sync)
 
         self.picked = False
         self._backup_prop = None
         self.id = QUuid.createUuid().toString()
-        #self._actor.AddObserver('PickEvent', self.pick)
-        #self._actor.AddObserver('UnPickEvent', self.unpick)
         self.initialised = False
 
+    def box_sync(self, caller : vtkBoxWidget, ev : str) -> None:
+        t = vtkTransform()
+        caller.GetTransform(t)
+        self._actor.SetUserTransform(t)
+
     @property
-    def actor(self) -> vtk.vtkActor:
+    def actor(self) -> vtkActor:
         return self._actor
 
-    def render(self, ren: vtk.vtkRenderer) -> None:
+    def render(self, ren: vtkRenderer) -> None:
         ren.AddActor(self._actor)
         ren.GetRenderWindow().Render()
     
-    def unrender(self, ren: vtk.vtkRenderer) -> None:
+    def unrender(self, ren: vtkRenderer) -> None:
         ren.RemoveActor(self._actor)
         ren.GetRenderWindow().Render()
 
-    def pick(self, caller: vtk.vtkRenderWindowInteractor, ev: str) -> None:
+    @calldata_type(VTK_OBJECT)
+    def pick(self, caller: vtkActor, ev: str, interactor : vtkRenderWindowInteractor = None) -> None:
         """set pick status
 
         Args:
             caller (vtkRenderWindowInteractor): object being observed
             ev (string): event type description
         """
-        if(not self.picked):
-            colors = vtk.vtkNamedColors()
-            actor_property = self.actor.GetProperty()
-            self._backup_prop = vtk.vtkProperty()
-            self._backup_prop.DeepCopy(actor_property)
+        if not interactor is None:
+            self._box_widget.SetInteractor(interactor)
+            if not self.picked :
+                self._box_widget.On()
+    
+    @calldata_type(VTK_OBJECT)
+    def unpick(self, caller: vtkActor, ev: str, interactor : vtkRenderWindowInteractor = None) -> None:
+        """set pick status
 
-            actor_property.SetColor(colors.GetColor3d('Red'))
-            actor_property.SetDiffuse(1.0)
-            actor_property.SetSpecular(0.0)
-            self._actor.ApplyProperties()
-
-            self.picked = True
-        else:
-            self.unpick(caller, ev)
-
-    def unpick(self, caller: vtk.vtkRenderWindowInteractor, ev: str) -> None:
-        if(self.picked and (self._backup_prop is not None)):
-            self._actor.GetProperty().DeepCopy(self._backup_prop)
-            self._actor.ApplyProperties()
-
-        self.picked = False
+        Args:
+            caller (vtkRenderWindowInteractor): object being observed
+            ev (string): event type description
+        """
+        if not interactor is None:
+            self._box_widget.SetInteractor(interactor)
+            if not self.picked :
+                self._box_widget.Off()
 
     def move(self, x: float, y: float, z: float) -> None:
         self._actor.AddPosition(x, y, z)
@@ -68,21 +84,23 @@ class PrintObject(VTKPythonAlgorithmBase):
         self._actor.RotateWXYZ(w, x, y, z)
         self.Modified()
 
-    def RequestData(self, request: str, inInfo: vtk.vtkInformation, outInfo: vtk.vtkInformation) -> bool:
-        input_poly = vtk.vtkPolyData.GetData(inInfo[0])
+    def RequestData(self, request: str, inInfo: vtkInformation, outInfo: vtkInformation) -> bool:
+        input_poly = vtkPolyData.GetData(inInfo[0])
 
         if not self.initialised:
-            mapper = vtk.vtkPolyDataMapper()
+            mapper = vtkPolyDataMapper()
             mapper.AddInputDataObject(input_poly)
             self._actor.SetMapper(mapper)
             self.initialised = True
+        
+        self._box_widget.PlaceWidget(self._actor.GetBounds())
 
-        output = vtk.vtkPolyData.GetData(outInfo)
+        output = vtkPolyData.GetData(outInfo)
 
-        transform = vtk.vtkTransform()
+        transform = vtkTransform()
         transform.SetMatrix(self._actor.GetMatrix())
 
-        coord_converter = vtk.vtkTransformPolyDataFilter()
+        coord_converter = vtkTransformPolyDataFilter()
         coord_converter.SetTransform(transform)
         coord_converter.AddInputData(input_poly)
         coord_converter.Update()
