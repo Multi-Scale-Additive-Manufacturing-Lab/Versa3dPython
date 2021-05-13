94308a337a91b5d3e75394c90783ee4baf8d68ac
marc wang <marc.wang@uwaterloo.ca>
2021-05-13 16:41:01 -0400

    Update test_voxelization.cpp
    basic shelling test

--- test/test_voxelization.cpp
+++ test/test_voxelization.cpp
@@ -1,28 +1,37 @@
 #include "catch2/catch.hpp"
 
-#include "vtkNew.h"
-#include "vtkSTLReader.h"
-#include "vtkXMLImageDataWriter.h"
 #include "VoxelizerFilter.h"
+#include "vtkSphereSource.h"
+#include "VoxelizerFilter.h"
+#include "VoxShelling.h"
+#include "vtkImageData.h"
+#include "vtkXMLImageDataWriter.h"
+#include "vtkSmartPointer.h"
 
 TEST_CASE("voxelize polydata", "[Process]")
 {
-    std::string inPath("./test_artifact/3DBenchy.stl");
-    std::string outPath("./output/3DBenchy.vti");
+    std::string outPath_2("./test/test_output/sphere_hollow.vti");
 
-    vtkNew<vtkSTLReader> reader;
-    reader->SetFileName(inPath.c_str());
-    reader->Update();
+    vtkSmartPointer<vtkSphereSource> sphereSource =
+        vtkSmartPointer<vtkSphereSource>::New();
+    sphereSource->SetRadius(20);
+    sphereSource->SetPhiResolution(30);
+    sphereSource->SetThetaResolution(30);
+    vtkSmartPointer<vtkPolyData> pd = sphereSource->GetOutput();
+    sphereSource->Update();
 
     vtkNew<VoxelizerFilter> voxF;
-    voxF->SetDpi(600, 600, 600);
+    voxF->SetInputData(pd);
     voxF->Update();
 
-    
+    vtkNew<VoxShelling> voxS;
+    voxS->SetInputConnection(voxF->GetOutputPort());
+    voxS->Update();
 
-    vtkNew<vtkXMLImageDataWriter> imWriter;
-    imWriter->SetInputConnection(voxF->GetOutputPort());
-    imWriter->SetFileName(outPath.c_str());
-    imWriter->Update();
-    imWriter->Write();
+    vtkSmartPointer<vtkXMLImageDataWriter> writer3 =
+        vtkSmartPointer<vtkXMLImageDataWriter>::New();
+    writer3->SetFileName(outPath_2.c_str());
+
+    writer3->SetInputData(voxS->GetOutput());
+    writer3->Write();
 }
